25146c20d038386ecd3d2831cfcfe841a5491fa9
metin <metin_yildirim@outlook.be>
2025-08-13 13:44:07 +0200

    create views

--- SudokuConsole/Program.cs
+++ SudokuConsole/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SudokuConsole.View;
+
+View4x4 view4 = new();
+Console.WriteLine("4x4");
+view4.Display();
+
+View9x9 view9 = new();
+Console.WriteLine("9x9");
+view9.Display();
+
+ViewSamourai viewSamourai = new();
+Console.WriteLine("Samourai");
+viewSamourai.Display();
---
+++ SudokuConsole/View/View4x4.cs
@@ -0,0 +1,17 @@
+﻿namespace SudokuConsole.View;
+
+internal class View4x4
+{
+    public void Display()
+    {
+        Console.WriteLine("""
+            +-----+-----+
+            | . . | . . |
+            | . . | . . |
+            +-----+-----+
+            | . . | . . |
+            | . . | . . |
+            +-----+-----+
+            """);
+    }
+}
