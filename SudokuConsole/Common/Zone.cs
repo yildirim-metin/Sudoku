271353975a73617e6729ca29e10d97484e170f6d
metin <metin_yildirim@outlook.be>
2025-08-19 14:57:52 +0200

    create zone class to store data that have a value

---
+++ SudokuConsole/Common/Zone.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace SudokuConsole.Common;
+
+internal class Zone
+{
+    private uint _values;
+
+    public Zone()
+    {
+        Init();
+    }
+
+    public Zone(uint values)
+    {
+        _values = values;
+    }
+
+    public void Init()
+    {
+        _values = 0;
+    }
+
+    public bool Contains(int value)
+    {
+        uint mask = (uint)1 << value - 1;
+        return (_values & mask) == mask;
+    }
+
+    public bool Add(int value)
+    {
+        if (!Contains(value))
+        {
+            uint mask = (uint)1 << value - 1;
+            _values |= mask;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+        // uint old = _values;
+        // values |= (uint)(1 << value - 1);
+        // return old != values;
+    }
+
+    public bool Remove(int value)
+    {
+        if (!Contains(value))
+        {
+            return false;
+        }
+        else
+        {
+            uint mask = (uint)1 << 0 << value - 1;
+            _values &= ~mask;
+            return true;
+        }
+
+        //uint old = _values;
+        //_values &= (uint)~(1 << value - 1);
+        //return old != _values;
+    }
+
+    public Zone Combine(Zone other)
+    {
+        uint combinedValues = _values | other._values;
+        return new Zone(combinedValues);
+    }
+
+    public int Count()
+    {
+        return BitOperations.PopCount(_values);
+    }
+
+    public override string ToString()
+    {
+        string binary = Convert.ToString(_values, 2).PadLeft(32,'0');
+        var octets = Enumerable.Range(0, 4)
+            .Select(i => binary.Substring(i * 8, 8))
+            .Select(b => $"{b.Substring(0, 4)}.{b.Substring(4, 4)}");
+
+        return string.Join(" ", octets);
+    }
+}
--- SudokuConsole/Program.cs
+++ SudokuConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using SudokuConsole.Contoller;
+﻿using SudokuConsole.Common;
+using SudokuConsole.Contoller;
 using SudokuConsole.Model;
 using SudokuConsole.View;
 
@@ -7,6 +8,7 @@ Console.WriteLine("""
     1. 4x4
     2. 9x9
     3. Samourai
+    4. Test binary
     """);
 
 Console.Write("Quel sudoku tu souhaite jouer ? :");
@@ -25,7 +27,7 @@ while (!isNumber)
     isNumber = int.TryParse(game, out gameChoice);
 }
 
-while (isNumber && (gameChoice < 0 || gameChoice > 3))
+while (isNumber && (gameChoice < 0 || gameChoice > 4))
 {
     Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine("Choisis une valeur entre 1 et 3 !");
@@ -60,13 +62,32 @@ switch(game)
         break;
 
     case "3":
+        //sudokuController.View = new ViewSamourai();
+        //sudokuController.Model = new SudokuModelSamourai();
+        //sudokuController.Start();
         Console.WriteLine("Pas encore implémenter");
+        break;
+
+    case "4":
+        Zone z = new();
+        Console.WriteLine(z);
+        z.Add(7);
+        z.Add(8);
+        Console.WriteLine(z);
+        z.Remove(3);
+        Console.WriteLine(z);
+
+        Zone z2 = new(); z2.Add(1); z2.Add(2);
+        Console.WriteLine(z2);
+        Zone z4 = z.Combine(z2);
+        Console.WriteLine(z4);
+        Console.WriteLine(z);
+
+        Zone z3 = new(0b1111111111);
+        Zone z5 = z2.Combine(z3);
+        Console.WriteLine(z5);
         break;
 
     default:
         break;
 }
-
-//sudokuController.View = new ViewSamourai();
-//sudokuController.Model = new SudokuModelSamourai();
-//sudokuController.Start();
